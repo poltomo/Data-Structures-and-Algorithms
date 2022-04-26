54d1aed373ca43e51f81f46b673d600ac1576189
Varun Nawathey <43126889+poltomo@users.noreply.github.com>
2022-04-26 09:04:42 -0700

    Completed Leetcode Problem

---
+++ LeetCode/LinkedListRemove.java
@@ -0,0 +1,27 @@
+// 203
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class LinkedListRemove {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0,head);
+        ListNode prev=sentinel;
+        ListNode curr=sentinel.next;
+        while (curr!=null){
+            if (curr.val==val){
+                prev.next=curr.next;
+            } else {
+                prev=curr;
+            }
+            curr=curr.next;
+        }
+        return sentinel.next;
+    }
+}
