7ed2a68497454d9187a2d0dbb62e66c4f6baa4aa
Varun Nawathey <43126889+poltomo@users.noreply.github.com>
2022-04-27 14:23:04 -0700

    Delete MergeSort.java

--- MergeSort.java
+++
@@ -1,9 +0,0 @@
-class MergeSort{
-    public static void mergeSort(int[] arr,l,r){
-
-    }
-    public static void main(String[] args){
-        int[] arr = {9,0,8,1,7,2,6,3,5,4};
-        
-    }
-}
