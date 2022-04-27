ad2fe5074d2425443f85cac4a36f214864dd2943
Varun Nawathey <43126889+poltomo@users.noreply.github.com>
2022-04-27 13:07:08 -0700

    Recursive Binary Search Algorithm

---
+++ BinarySearchRec.java
@@ -0,0 +1,26 @@
+class BinarySearchRec{
+    public static int binSearch(int[] arr, int target, int min, int max){
+    int mid = (min+max)/2;
+    if (max<min){
+      return -1;
+    }
+    if (target==arr[mid]){
+      return mid;
+    }
+    else if (target<arr[mid]){
+      return binSearch(arr, target, min, mid-1);
+    }
+    else if (target>arr[mid]){
+      return binSearch(arr, target, mid+1, max);
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+      int[] arr={0,1,2,3,4,5,6,7,8,9};
+      //         0,1,2,3,4,5,6,7,8,9
+      for (int num:arr){
+          System.out.println(binSearch(arr,num,0,arr.length));
+      }
+  }
+}
