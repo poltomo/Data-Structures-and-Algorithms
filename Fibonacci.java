cfcfdeeeab9ec71f04ad36428a6782483c0c15cb
Varun Nawathey <vnaw4988@gmail.com>
2022-04-29 00:01:52 -0700

    added comment

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,8 +3,8 @@ public class Fibonacci {
         return new int[0];
     }
     public static void main(String[] args) {
-        // 0 1 1 2 3 5 8 13 21
-        // 0 1 2 3 4 5 6 7  8
+        // 0 1 1 2 3 5 8 13 21 34
+        // 0 1 2 3 4 5 6 7  8  9
         System.out.println(fibonacci(6));
     }
 }
