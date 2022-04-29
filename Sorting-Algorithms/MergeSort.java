4fe3977b0f9a0eb7cb889c81f32d5ec9cefb1fe9
Varun Nawathey <vnaw4988@gmail.com>
2022-04-28 23:41:26 -0700

    Completed Merge Sort Algorithm

--- Sorting-Algorithms/MergeSort.java
+++ Sorting-Algorithms/MergeSort.java
@@ -18,10 +18,43 @@ class MergeSort{
 
         mergeSort(arrLeft);
         mergeSort(arrRight);
+
+        merge(arr, arrLeft, arrRight); // merge
+    }
+    private static void merge(int[] arr, int [] arrLeft, int[] arrRight){
+        int nLeft = arrLeft.length;
+        int nRight = arrRight.length;
+
+        int i=0, j=0, k=0;
+
+        while (i<nLeft && j<nRight){
+            if (arrLeft[i]<=arrRight[j]){
+                arr[k]=arrLeft[i];
+                i++;
+            }
+            else {
+                arr[k]=arrRight[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<nLeft){
+            arr[k]=arrLeft[i];
+            i++;
+            k++;
+        }
+        while (j<nRight){
+            arr[k]=arrRight[j];
+            j++;
+            k++;
+        }
     }
     public static void main(String[] args){
-        int[] arr = {9,0,8,1,7,2,6,3,5,4};\
+        int[] arr = {9,0,8,1,7,2,6,3,5,4};
         mergeSort(arr);
-        System.out.println(arr);
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
     }
 }
