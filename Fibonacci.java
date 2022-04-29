a6852bc8509c4bc99505f050e39220bddb9e849a
Varun Nawathey <vnaw4988@gmail.com>
2022-04-28 23:55:06 -0700

    fibonnaci problem

---
+++ Fibonacci.java
@@ -0,0 +1,10 @@
+public class Fibonacci {
+    public static int [] fibonacci(int n){
+        return new int[0];
+    }
+    public static void main(String[] args) {
+        // 0 1 1 2 3 5 8 13 21
+        // 0 1 2 3 4 5 6 7  8
+        System.out.println(fibonacci(6));
+    }
+}
