e1b16a8fc0776f6672b48fbb11cbd7c654cbf4d4
Varun Nawathey <vnaw4988@gmail.com>
2022-04-30 17:29:04 -0700

    Refactored Fibonacci number program

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,16 @@
 public class Fibonacci {
-    public static int [] fibonacci(int n){
-        return new int[0];
+    public static int fibonacci(int n){
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
     public static void main(String[] args) {
         // 0 1 1 2 3 5 8 13 21 34
         // 0 1 2 3 4 5 6 7  8  9
-        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(2));
     }
 }
