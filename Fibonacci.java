48645b8335a367d10745cd32a0f6c876a6ea0507
Varun Nawathey <vnaw4988@gmail.com>
2022-04-30 17:50:39 -0700

    refactored

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,7 @@
 class Fibonacci {
     public static int fibonacci(int n){
-        if (n==0){
-            return 0;
-        }
-        if (n==1){
-            return 1;
+        if (n<2){
+            return n;
         }
         return fibonacci(n-1) + fibonacci(n-2);
     }
